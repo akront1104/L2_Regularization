a294a66d79115388bba8d78ce3e3f653f4c93986
Angela Krontiris <31998453+akront1104@users.noreply.github.com>
2018-09-27 23:17:48 -0400

    Add files via upload

---
+++ CS6930_HW1_L2_Regularization.py
@@ -0,0 +1,5 @@
+Python 3.6.1 (v3.6.1:69c0db5050, Mar 21 2017, 01:21:04) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
